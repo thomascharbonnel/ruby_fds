c919dc55944f5033dacc26c290a3173ade90e61e
Thomas Charbonnel <thomas@charbonnel.eu>
2016-09-28 19:34:54 +0200

    Correct deletion and indexing

--- lib/fds/unordered_set.rb
+++ lib/fds/unordered_set.rb
@@ -1,11 +1,15 @@
 class FDS::UnorderedSet
   def initialize
-    @data = Hash.new { |h, e| h[e] = h.size }
+    @index = 0
+    @data = Hash.new do |h, e|
+      h[e] = @index
+      @index += 1
+    end
   end
 
   def add(e); @data[e]; self; end
   def find_index(e); @data[e] if @data.has_key?(e); end
-  def remove(e); @data[e] = nil; end
+  def remove(e); @data.delete(e); self; end
   def size; @data.size; end
 
   [:to_s, :first, :last, :to_a].each do |function|
